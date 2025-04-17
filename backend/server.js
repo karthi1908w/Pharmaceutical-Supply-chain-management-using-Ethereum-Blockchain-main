e8a0065e6b03d7b34c590cedc8a81efb4b5f1a05
MADHANESHWARAN M S <147685104+MADHANESH@users.noreply.github.com>
2025-04-17 17:26:05 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -25,7 +25,7 @@ const io = new Server(server, {
 
 app.use(express.json());
 app.use(cors({
-  origin: "https://pharmaceutical-supply.vercel.app/", // ✅ frontend domain
+  origin: "https://pharmaceutical-supply.vercel.app", // ✅ frontend domain
   credentials: true
 }));
 
